70842c7bdac67d32139f1dd78fb83e0ea86eb5c2
pumpkin <51853190+zhangnangua@users.noreply.github.com>
2025-08-01 16:46:42 +0800

    Update WebViewCacheInterceptor.kt

--- JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/WebViewCacheInterceptor.kt
+++ JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/WebViewCacheInterceptor.kt
@@ -7,26 +7,4 @@ import android.webkit.WebView
 
 
 object WebViewCacheInterceptor : IWebViewRequestInterceptor {
-
-    @SuppressLint("StaticFieldLeak")
-    var interceptorGlobal: WebViewCacheInterceptorGlobal? = null
-
-
-    fun init(builder: WebViewCacheInterceptorGlobal.Builder) {
-        interceptorGlobal = builder.build()
-    }
-
-    override fun interceptRequest(request: WebResourceRequest?): WebResourceResponse? =
-        interceptorGlobal?.interceptRequest(request)
-
-    override fun interceptRequest(url: String?): WebResourceResponse? =
-        interceptorGlobal?.interceptRequest(url)
-
-    override fun loadUrl(webView: WebView, url: String) {
-        interceptorGlobal?.loadUrl(webView, url)
-    }
-
-    override fun enableCache(enable: Boolean) {
-        interceptorGlobal?.enableCache(enable)
-    }
 }
