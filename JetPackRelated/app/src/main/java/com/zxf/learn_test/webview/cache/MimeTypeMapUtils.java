23b28db82ba1d5301a5484d1f08d21e2321bf2b1
pumpkin <51853190+zhangnangua@users.noreply.github.com>
2025-08-01 16:27:18 +0800

    Update MimeTypeMapUtils.java
    deal

--- JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/MimeTypeMapUtils.java
+++ JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/MimeTypeMapUtils.java
@@ -3,48 +3,6 @@ package com.zxf.learn_test.webview.cache;
 import android.text.TextUtils;
 import android.webkit.MimeTypeMap;
 
-/**
- * 作者： 张先锋
- * 创建时间： 2022/03/17 15:31
- * 版本： [1.0, 2022/03/17]
- * 版权： 国泰新点软件股份有限公司
- * 描述： MimeType辅助处理
- */
 public class MimeTypeMapUtils {
 
-    public static String getFileExtensionFromUrl(String url) {
-        url = url.toLowerCase();
-        if (!TextUtils.isEmpty(url)) {
-            int fragment = url.lastIndexOf('#');
-            if (fragment > 0) {
-                url = url.substring(0, fragment);
-            }
-
-            int query = url.lastIndexOf('?');
-            if (query > 0) {
-                url = url.substring(0, query);
-            }
-
-            int filenamePos = url.lastIndexOf('/');
-            String filename =
-                    0 <= filenamePos ? url.substring(filenamePos + 1) : url;
-
-            // if the filename contains special characters, we don't
-            // consider it valid for our matching purposes:
-            if (!filename.isEmpty()) {
-                int dotPos = filename.lastIndexOf('.');
-                if (0 <= dotPos) {
-                    return filename.substring(dotPos + 1);
-                }
-            }
-        }
-
-        return "";
-    }
-    public static String getMimeTypeFromUrl(String url) {
-        return  MimeTypeMap.getSingleton().getMimeTypeFromExtension(getFileExtensionFromUrl(url));
-    }
-    public static String getMimeTypeFromExtension(String extension) {
-        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
-    }
 }
