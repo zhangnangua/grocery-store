58dbca89c81295cbd075204b087b1bf3aae6ea01
pumpkin <51853190+zhangnangua@users.noreply.github.com>
2025-08-01 16:26:13 +0800

    Update NetUtils.java
    deal

--- JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/NetUtils.java
+++ JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/NetUtils.java
@@ -13,45 +13,4 @@ import java.util.Map;
 
 public class NetUtils {
 
-    public static boolean isConnected(Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context.getApplicationContext()
-                .getSystemService(Activity.CONNECTIVITY_SERVICE);
-        NetworkInfo info = cm.getActiveNetworkInfo();
-        return info != null && info.isConnected();
-    }
-
-    public static  String getOriginUrl(String referer) {
-        String ou = referer;
-        if (TextUtils.isEmpty(ou)) {
-            return "";
-        }
-        try {
-            URL url = new URL(ou);
-            int port = url.getPort();
-            ou = url.getProtocol() + "://" + url.getHost() + (port == -1 ? "" : ":" + port);
-        } catch (Exception e) {
-        }
-        return ou;
-    }
-
-    public static Map<String,String> multimapToSingle(Map<String, List<String>> maps){
-
-        StringBuilder sb = new StringBuilder();
-        Map<String,String> map = new HashMap<>();
-        for (Map.Entry<String, List<String>> entry: maps.entrySet()) {
-            List<String> values = entry.getValue();
-            sb.delete(0,sb.length());
-            if (values!=null&&values.size()>0){
-                for (String v:values) {
-                    sb.append(v);
-                    sb.append(";");
-                }
-            }
-            if (sb.length()>0){
-                sb.deleteCharAt(sb.length()-1);
-            }
-            map.put(entry.getKey(),sb.toString());
-        }
-        return map;
-    }
 }
