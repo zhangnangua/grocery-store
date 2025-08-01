0a905d10401df48512e9649f4f6d3f1d07019bb4
pumpkin <51853190+zhangnangua@users.noreply.github.com>
2025-08-01 16:39:18 +0800

    Update WebViewCacheInterceptor.kt
    deal

--- JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/WebViewCacheInterceptor.kt
+++ JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/WebViewCacheInterceptor.kt
@@ -5,13 +5,7 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 
-/**
- * 作者： 张先锋
- * 创建时间： 2022/03/17 15:31
- * 版本： [1.0, 2022/03/17]
- * 版权： 国泰新点软件股份有限公司
- * 描述： WebViewRequestInterceptor拦截器功能具体实现类
- */
+
 object WebViewCacheInterceptor : IWebViewRequestInterceptor {
 
     @SuppressLint("StaticFieldLeak")
