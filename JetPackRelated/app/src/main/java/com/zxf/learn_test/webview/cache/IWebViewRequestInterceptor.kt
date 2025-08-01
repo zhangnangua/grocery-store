937fb9d11b509bb486fa6bcf6c3baa0cee3f332d
pumpkin <51853190+zhangnangua@users.noreply.github.com>
2025-08-01 16:46:58 +0800

    Update IWebViewRequestInterceptor.kt

--- JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/IWebViewRequestInterceptor.kt
+++ JetPackRelated/app/src/main/java/com/zxf/learn_test/webview/cache/IWebViewRequestInterceptor.kt
@@ -8,24 +8,4 @@ import android.webkit.WebView
 
 internal interface IWebViewRequestInterceptor {
 
-    /**
-     * 请求拦截
-     */
-    fun interceptRequest(request: WebResourceRequest?): WebResourceResponse?
-    fun interceptRequest(url: String?): WebResourceResponse?
-
-    /**
-     * loadUrl sdk<21时 需要通过它手动设置 相关请求头 Origin Referer UserAgent
-     * Origin 用于指明当前请求来自于哪个站点 格式：https://baidu.com
-     * Referer 告诉服务器我是从哪个页面链接过来的 格式：一个完成的url
-     * UserAgent
-     */
-    fun loadUrl(webView: WebView, url: String)
-
-    /**
-     * 开始强制缓存
-     * true 开启缓存
-     * false webView正常资源加载逻辑
-     */
-    fun enableCache(enable: Boolean)
 }
